c69c3f1871f9b865bcc43d8df5ad3347c91aeebe
Neama Ibrahim <neamaibrahim@Neamas-MacBook-Air.local>
2026-09-22 16:12:41 +0300

    edit

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -23,7 +23,7 @@ function Notes(){
         try{
             const token = localStorage.getItem("token");
             setLoading(true);
-            const data = await fetch(`https://study-hub-production-4e64.up.railway.app/api/notes?search=${encodeURIComponent(query)}`,{
+            const data = await fetch(`https://study-hub-brown-mu.vercel.app/api/notes?search=${encodeURIComponent(query)}`,{
   method: "GET",
   headers: {
     "Authorization": `Bearer ${token}`,
@@ -52,7 +52,7 @@ function Notes(){
         const token = localStorage.getItem("token");
         if(!title.trim()||!content.trim()) return;
         try{
-        const response = await fetch('https://study-hub-production-4e64.up.railway.app/api/notes', {
+        const response = await fetch('https://study-hub-brown-mu.vercel.app/api/notes', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
